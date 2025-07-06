ff362acbed04519790a0d19ad3d607fbda3e1b11
Oskar <httpllkh@gmail.com>
2025-07-06 23:28:39 +0500

    second task

--- stream-task/stream-task.js
+++ stream-task/stream-task.js
@@ -1,2 +1,23 @@
 const fs = require("fs")
+const { Transform } = require("stream")
 
+const sumTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        const str = chunk.toString()
+        const numbers = str.match(/\d+/g)
+
+        let sumChank = 0
+        for (num in numbers) {
+            sumChank += parseInt(num)
+        }
+        this.push(sumChank + '/n')
+        callback()
+    }
+})
+
+const readStream = fs.createReadStream("input.txt")
+const writeStream = fs.createWriteStream("out.txt")
+
+readStream
+    .pipe(sumTransform)
+    .pipe(writeStream)
